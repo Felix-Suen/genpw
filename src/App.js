9de3b59615fc99da28c0706ffc75ba0c72d71132
Felix Suen <felix.suen2000@gmail.com>
2019-11-15 11:32:33 -0500

    special character

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import './App.css';
 
-const chara = 'abcdefghijklmnopqrstuvwxyz1234567890';
+const chara = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!\"#$%\&\'()*+,-./:;<=>?@[\\]^_`{|}~';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      skoot: 'du hello',
+      Password: 'du hello',
     }
   }
   
   onClickButton = () => {
     var stringer = '';
     for (var i = 0; i < 16; i++) {
-      stringer += chara[Math.floor(Math.random() * 35) + 1];
+      stringer += chara[Math.floor(Math.random() * 92) + 1];
     }
     this.setState({
-      skoot: stringer,
+      Password: stringer,
     });
-    console.log(this.state.skoot);
   } 
 
   render() {
@@ -28,7 +27,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <button className="button" onClick={this.onClickButton}>Gen Password</button>
-          <p>{this.state.skoot}</p>
+          <p>{this.state.Password}</p>
         </header>
       </div>
     );
